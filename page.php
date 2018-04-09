e98324330f1d3c5ba13ce50a8788555212eadca3
Nina Klemencic <nina4klemencic@gmail.com>
2018-04-09 12:14:29 +0200

    css

--- page.php
+++ page.php
@@ -1,8 +1,10 @@
 <?php 
+  get_header();
   while(have_posts()) {
     the_post(); ?>
     <h1> This is a page </h1>
     <h2><?php the_title(); ?></a></h2>
     <?php the_content(); ?>
   <?php }
+  get_footer();
 ?>
